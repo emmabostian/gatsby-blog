fd2f0fa80f223f47922a33fd1b8d747454f74a45
Emma Bostian <Emma.Bostian@logmein.com>
2018-10-29 13:31:39 +0100

    Removing external stylesheet for home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,10 @@ import Layout from '../components/layout'
 
 import homeGraphic from '../images/home-graphic.svg'
 
-import './index.css';
-
 const IndexPage = () => (
   <Layout>
-    <img className='home__graphic' src={homeGraphic} alt='Woman at computer' />
-    <h1>Hi, I’m Emma. I’m a Front-end Developer, Designer, & cat mom.</h1>
+    <img style={{ marginTop: '50px' }} src={homeGraphic} alt='Woman at computer' />
+    <h1 style={{ marginTop: '50px' }}>Hi, I’m Emma. I’m a Front-end Developer, Designer, & cat mom.</h1>
     <p>I love all things web development & design. I was born and raised in Upstate New York. After a few years in Austin, Texas I sold everything and moved to Karlsruhe, Germany.</p>
     <p>Feel free to send me a message on Twitter!</p>
   </Layout>
