936bcd891a5505a1d1b4afdf5276bf150ac8a536
Emma Bostian <Emma.Bostian@logmein.com>
2018-10-29 13:55:20 +0100

    About section

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => (
           style={{
             margin: '0 auto',
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
+            padding: '0px 20px 1.45rem',
             paddingTop: 0,
           }}
         >
