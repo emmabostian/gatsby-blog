95eb2a4c6ddd3c4721fb6f15aa62d785d48f8763
Emma Bostian <Emma.Bostian@logmein.com>
2018-10-29 11:22:55 +0100

    Adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
         logo: 'src/images/logo.svg'
       },
     },
@@ -35,6 +34,7 @@ module.exports = {
           `'Montserrat', sans-serif`,
         ]
       }
-    }
+    },
+    `gatsby-plugin-favicon`
   ],
 }
