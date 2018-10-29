1adef3687d67b842230c47aad9a06fb17d078dfc
Emma Bostian <Emma.Bostian@logmein.com>
2018-10-29 13:30:20 +0100

    Adding home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import homeGraphic from '../images/home-graphic.svg'
+
+import './index.css';
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hi my name is Emma Wedekind</h1>
-    <p>Welcome to my portfolio.</p>
+    <img className='home__graphic' src={homeGraphic} alt='Woman at computer' />
+    <h1>Hi, I’m Emma. I’m a Front-end Developer, Designer, & cat mom.</h1>
+    <p>I love all things web development & design. I was born and raised in Upstate New York. After a few years in Austin, Texas I sold everything and moved to Karlsruhe, Germany.</p>
+    <p>Feel free to send me a message on Twitter!</p>
   </Layout>
 )
 
